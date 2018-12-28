23d8474f09607be92f4e6963bccd281c7d66d854
Kirill <kirsh.ru@yandex.ru>
2018-12-28 12:57:54 +0700

    fix

--- routes/polomki.js
+++ routes/polomki.js
@@ -1,20 +1,20 @@
 var express = require('express')
 var router = express.Router()
-const ServiceList = require('../database/models/ServiceList')
-const Wagon = require('../database/models/Wagon')
-const StatusOfServiceList = require('../database/models/StatusOfServiceList')
-const Client = require('../database/models/Client')
+const ServiceListBreakdown = require('../database/models/ServiceListBreakdown')
+const Breakdown = require('../database/models/Breakdown')
+const BreakdownType = require('../database/models/BreakdownType')
 const Sequelize = require('sequelize')
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
-  let wagon = await ServiceList.findAll({
-    include: [{ model: Wagon, as: 'Wagon' },{ model: StatusOfServiceList, as: 'StatusOfServiceList' },{ model: Client, as: 'Client' }]
+  let polomka = await ServiceListBreakdown.findAll({
+    where:{ServiceListFK:req.query.id},
+    include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
   })
     .catch((err) => {
       console.log(err)
     })
-  res.render('wagon', {
-    wagons:wagon
+  res.render('polomki', {
+    usluga:polomka
   })
 })
 
